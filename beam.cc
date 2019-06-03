0b3a9302b67cc8257c9ffc0c01914d6802a93ed1
Josh Erickson <ponderosa@imaginaryphotons.com>
2019-06-03 15:09:16 -0400

    Add initial files

---
+++ beam.cc
@@ -0,0 +1,5 @@
+//
+// Created by ponderosa on 6/3/19.
+//
+
+#include "beam.h"
---
+++ beam.h
@@ -0,0 +1,14 @@
+//
+// Created by ponderosa on 6/3/19.
+//
+
+#ifndef TWILIGHT_BEAM_H
+#define TWILIGHT_BEAM_H
+
+
+class beam {
+
+};
+
+
+#endif //TWILIGHT_BEAM_H
